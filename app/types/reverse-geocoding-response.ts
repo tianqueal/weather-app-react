e5c9faa27b1fffc4e1254e37d8e1935acc1cd0be
Christian <aqchristian12@gmail.com>
2025-01-24 19:52:06 +0100

    update: reverse geocoding interface

--- app/types/reverse-geocoding-response.ts
+++ app/types/reverse-geocoding-response.ts
@@ -1,32 +1,34 @@
 export interface ReverseGeocodingResponse {
-  place_id: number;
-  licence: string;
-  osm_type: string;
-  osm_id: number;
-  lat: string;
-  lon: string;
-  class: string;
-  type: string;
-  place_rank: number;
-  importance: number;
-  addresstype: string;
-  name: string;
-  display_name: string;
-  address: Address;
-  boundingbox: string[];
+  place_id?: number;
+  licence?: string;
+  osm_type?: string;
+  osm_id?: number;
+  lat?: string;
+  lon?: string;
+  class?: string;
+  type?: string;
+  place_rank?: number;
+  importance?: number;
+  addresstype?: string;
+  name?: string;
+  display_name?: string;
+  address?: Address;
+  boundingbox?: string[];
+  error?: string;
 }
 
 export interface Address {
-  road: string;
+  road?: string;
   neighbourhood: string;
-  hamlet: string;
-  civil_parish: string;
-  town: string;
-  county: string;
-  province: string;
-  "ISO3166-2-lvl6": string;
-  state: string;
-  "ISO3166-2-lvl4": string;
+  hamlet?: string;
+  civil_parish?: string;
+  town?: string;
+  city?: string;
+  county?: string;
+  province?: string;
+  "ISO3166-2-lvl6"?: string;
+  state?: string;
+  "ISO3166-2-lvl4"?: string;
   postcode: string;
   country: string;
   country_code: string;
