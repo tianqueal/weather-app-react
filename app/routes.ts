e719a0c3cecd3ed368f8d83362c1b25828b177a6
Christian Alvarado <tianqueal@gmail.com>
2025-05-27 22:41:14 +0200

    feat: update route for current weather page

--- app/routes.ts
+++ app/routes.ts
@@ -2,5 +2,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-  route("today", "routes/today/index.tsx"),
+  route("current", "routes/current/index.tsx"),
 ] satisfies RouteConfig;
