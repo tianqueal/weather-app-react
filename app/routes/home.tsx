c279d64f7e120207aaa2ba55bc09d9d59c826443
Christian Alvarado <tianqueal@gmail.com>
2025-05-27 22:40:34 +0200

    feat: update meta tags to use dynamic product name

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,11 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import { productName } from "package.json";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: productName },
+    { name: "description", content: `Welcome to ${productName}!` },
   ];
 }
 
